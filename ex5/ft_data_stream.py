7a7ae058c24a3da099957f601033abf9d48bdc0d
Matthias Voiturier <matthias.voiturier@hotmail.com>
2026-07-02 17:53:58 +0200

    wip

--- ex5/ft_data_stream.py
+++ ex5/ft_data_stream.py
@@ -27,6 +27,13 @@ def gen_event() -> typing.Generator[tuple[str, str], None, None]:
         random_action: int = random.choice(ACTIONS)
         yield (random_player, random_action)
 
+def consume_event(lst: list[tuple[str,str]]) -> typing.Generator[tuple[str,str], None, None]:
+    while(len(lst) != 0):
+        random_index: int = random.randint(0,len(lst) - 1)
+        item: tuple[str,str] = lst[random_index]
+        lst.pop(random_index)
+        yield item
+
 
 def main() -> None:
     event_generator = gen_event()
@@ -40,6 +47,10 @@ def main() -> None:
     lst: list[tuple[str, str]] = [next(event_generator) for _ in range(10)]
     print(lst)
 
+    delete_generator = consume_event(lst)
 
+    for index in delete_generator:
+        print(next(delete_generator)) 
+    print("Over", lst)
 if __name__ == "__main__":
     main()
