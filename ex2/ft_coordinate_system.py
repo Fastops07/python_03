807639d5c66164ddfe176402b693ddedb2bcbb71
Matthias Voiturier <matthias.voiturier@hotmail.com>
2026-07-03 13:47:44 +0200

    correction ex2

--- ex2/ft_coordinate_system.py
+++ ex2/ft_coordinate_system.py
@@ -4,16 +4,34 @@ import math
 def get_player_pos() -> tuple[float, float, float]:
     while True:
         try:
-            coordonates_str: list[str] = input(
+            x_str: str
+            y_str: str
+            z_str: str
+            x_str, y_str, z_str = input(
                 "Enter new coordinates as floats in format 'x,y,z': "
             ).split(",")
-            x: float
-            y: float
-            z: float
-            x, y, z = (float(pos.strip()) for pos in coordonates_str)
-            return (x, y, z)
         except ValueError:
             print("Invalid syntax")
+            continue
+
+        try:
+            x = float(x_str.strip())
+        except ValueError as err:
+            print(f"Error on parameter '{x_str.strip()}': {err}")
+            continue
+
+        try:
+            y = float(y_str.strip())
+        except ValueError as err:
+            print(f"Error on parameter '{y_str.strip()}': {err}")
+            continue
+
+        try:
+            z = float(z_str.strip())
+        except ValueError as err:
+            print(f"Error on parameter '{z_str.strip()}': {err}")
+            continue
+        return (x, y, z)
 
 
 def calculate_distance(
@@ -26,7 +44,8 @@ def calculate_distance(
 
 def main() -> None:
     print("=== Game Coordinate System ===")
-
+    print()
+    print("Get a first set of coordinates")
     pos_p1: tuple[float, float, float] = get_player_pos()
     print(f"Got a first tuple: {pos_p1}")
     print(f"It includes: X={pos_p1[0]}, Y={pos_p1[1]}, Z={pos_p1[2]}")
@@ -34,13 +53,17 @@ def main() -> None:
     center: tuple[float, float, float] = (0.0, 0.0, 0.0)
     distance_from_center: float = round(calculate_distance(pos_p1, center), 4)
     print(f"Distance to center: {distance_from_center}")
+    print()
 
     print("Get a second set of coordinates")
     pos_p2: tuple[float, float, float] = get_player_pos()
     distance_between_points: float = round(
         calculate_distance(pos_p1, pos_p2), 4
     )
-    print(f"Distance between the two points: {distance_between_points}")
+    print(
+        "Distance between the 2 sets of coordinates: "
+        f"{distance_between_points}"
+    )
 
 
 if __name__ == "__main__":
